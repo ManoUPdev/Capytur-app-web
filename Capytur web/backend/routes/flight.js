cb08eb64b987b7084941bbcf32a4c48d9d47a574
ManoUPdev <emanuelcgalindo@hotmail.com>
2025-08-22 10:43:32 -0300

    some

--- Capytur web/backend/routes/flight.js
+++ Capytur web/backend/routes/flight.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-// Retorna todos os voos (mock ou do banco)
-router.get('/', (req, res) => {
+router.get('/',(req,res) =>  {
     res.json([
-        { id: 1, from: "São Paulo", to: "Rio de Janeiro", price: 200 },
-        { id: 2, from: "São Paulo", to: "Belo Horizonte", price: 250 }
+        { id: 1, from: "Sao Paulo", to: "Rio de Janeiro", price: 200},
+        { Id: 2, from: "Sao Paulo", to: "Belo Horizonte", price: 250}
+
     ]);
+
 });
 
-// Retorna detalhes de um voo
 router.get('/:id', (req, res) => {
     const flightId = req.params.id;
-    res.json({ id: flightId, from: "São Paulo", to: "Rio de Janeiro", price: 200, duration: "1h10m" });
-});
+    res.json({ id: flightId, from: "Sao Paulo", to: "Rio de Janeiro", price:200, duration: "35min"});
 
+});
 module.exports = router;
--- Capytur web/backend/routes/reservation.js
+++ Capytur web/backend/routes/reservation.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-// Retorna todos os voos (mock ou do banco)
-router.get('/', (req, res) => {
-    res.json([
-        { id: 1, from: "São Paulo", to: "Rio de Janeiro", price: 200 },
-        { id: 2, from: "São Paulo", to: "Belo Horizonte", price: 250 }
-    ]);
-});
+router.post('/', (req, res) => {
+    
+const reservation = req.body;
 
-// Retorna detalhes de um voo
-router.get('/:id', (req, res) => {
-    const flightId = req.params.id;
-    res.json({ id: flightId, from: "São Paulo", to: "Rio de Janeiro", price: 200, duration: "1h10m" });
+res.json({message: "Reserva realizada com sucesso!", reservation});
 });
 
 module.exports = router;
