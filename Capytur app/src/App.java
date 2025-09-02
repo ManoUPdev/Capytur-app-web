2f2ee4c628971c9e37c74bb6a8593c3667344345
ManoUPdev <141835220+ManoUPdev@users.noreply.github.com>
2025-09-02 11:17:19 -0300

    Delete Capytur app directory

--- Capytur app/src/App.java
+++
@@ -1,8 +0,0 @@
-import br.edu.up.telas.CapyturApp;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        CapyturApp menu = new CapyturApp();
-        menu.iniciar();
-    }
-}
